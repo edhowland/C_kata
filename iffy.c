10f2261ff887c0c28a50d61f63841a11725c99a3
Ed Howland <ed.howland@gmail.com>
2020-08-07 17:16:34 -0400

    moved second option into the then branch, Hello World is now the default branch

--- iffy.c
+++ iffy.c
@@ -3,9 +3,9 @@
 
 int main(int argc, char *argv[]) {
   if (argc > 1) {
-    printf("Hello World!\n"); 
-  } else {
     printf("second option\n");
+  } else {
+    printf("Hello World!\n"); 
   }
 
   return 0;
