6c734b685fd20476baea9ea1e45fe8ae3e2cbdf5
Ed Howland <ed.howland@gmail.com>
2020-08-24 19:57:11 -0400

    added exit_failure.c no_magic.c for no magic numbers

---
+++ exit_failure.c
@@ -0,0 +1,8 @@
+// exit_failure.c - exit with bad juju
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+  return EXIT_FAILURE;
+}
---
+++ no_magic.c
@@ -0,0 +1,8 @@
+// no_magic.c - Do not use Magic Numbers
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+  return EXIT_SUCCESS;
+}
