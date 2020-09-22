0b0e9fcd18b1fe08c75631d222674d8f2dcc561b
Ed Howland <ed.howland@gmail.com>
2020-09-22 12:54:17 -0400

    renamed exit_failure.c to no_magic.c

--- nomagic/no_magic.c
+++ nomagic/no_magic.c
@@ -1,8 +1,13 @@
-// no_magic.c - Do not use Magic Numbers
+// nomagic.c : No magic numbers
 #include <stdio.h>
+// include stdlib.h to get EXIT_ constants
 #include <stdlib.h>
 
-int main(void) {
-
-  return EXIT_SUCCESS;
+// check that we get no arguments passed
+int main(int argc, char **argv) {
+  if (argc > 1) {
+    return EXIT_FAILURE;
+  } else {
+    return EXIT_SUCCESS;
+}
 }
